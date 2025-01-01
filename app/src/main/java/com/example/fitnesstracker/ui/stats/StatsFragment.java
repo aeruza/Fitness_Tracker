bc1e36799405c861c67bc9cdd6219aaf2e14f3b4
Sean <118966410+aeruza@users.noreply.github.com>
2025-01-02 01:07:28 +0800

    style(text): Removed the duplicate on fragment titles for less condensed UI

--- app/src/main/java/com/example/fitnesstracker/ui/stats/StatsFragment.java
+++ app/src/main/java/com/example/fitnesstracker/ui/stats/StatsFragment.java
@@ -1,19 +1,15 @@
 package com.example.fitnesstracker.ui.stats;
 
-import android.icu.text.DateFormat;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import com.example.fitnesstracker.databinding.FragmentStatsBinding;
-import com.example.fitnesstracker.databinding.FragmentTrackerBinding;
-import com.example.fitnesstracker.ui.tracker.TrackerViewModel;
 
-import java.util.Date;
+import com.example.fitnesstracker.databinding.FragmentStatsBinding;
 
 public class StatsFragment extends Fragment {
 
@@ -24,19 +20,10 @@ private FragmentStatsBinding binding;
         StatsViewModel statsViewModel =
                 new ViewModelProvider(this).get(StatsViewModel.class);
 
-    binding = FragmentStatsBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentStatsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        final TextView textView = binding.textStats;
-        statsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
-    }
-
-    public String getDate() {
-        Date d = new Date();
-        return "";
-        // String currentDate = String.format(String.valueOf(d.getDate()));
-        // return currentDate;
     }
 
     @Override
